f957845738e3d7e71547e8bcbcd9b0e5e33f8c04
erio <erio@akanuma>
2020-05-08 22:44:45 +0900

    update ??

--- src/f103_unit1/main.cpp
+++ src/f103_unit1/main.cpp
@@ -3,31 +3,55 @@
 
 DigitalOut myled(PC_13);
 
-Mon mon(USBTX, USBRX);
-Param param0;
+// MonBase mon(USBTX, USBRX);
+RawSerial serial(USBTX, USBRX, 115200);
 
-void led(std::vector<std::string> command)
+std::string led(std::vector<std::string> command)
 {
+  std::string result = "not match";
   if (command[1] == "set")
   {
     if (command[2] == "0")
+    {
       myled = 0;
+      result = "set on";
+    }
     else
+    {
       myled = 1;
+      result = "set off";
+    }
+  }
+  return result;
+}
+
+void dummy(void)
+{
+  while (true)
+  {
+    thread_sleep_for(500);
+    serial.printf("aaaa\n");
   }
 }
 
 int main()
 {
-  mon.register_func("led", callback(led));
-  mon.register_func("param", callback(&param0, &Param::monCallback));
+  // mon.start();
 
-  param0.register_int("PID_P", 10);
-  param0.register_int("PID_I", 20);
+  Thread thread1;
+  // thread1.start(callback(&mon, &MonBase::dummy));
+  thread1.start(dummy);
+
+  // mon.register_func("led", callback(led));
+  // mon.register_func("param", callback(&param0, &Param::monCallback));
+
+  // param0.register_int("PID_P", 10);
+  // param0.register_int("PID_I", 20);
 
   while (1)
   {
     thread_sleep_for(100);
-    mon.process();
+    // mon.process();
+    // myled = !myled;
   }
 }
